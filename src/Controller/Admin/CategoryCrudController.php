49cf2c41ccab6b31d66c239ca7aaa808b0b33441
Kristof <christophecoupigny@gmail.com>
2022-10-04 16:50:36 +0200

    Translation and configuration of categories pages

--- src/Controller/Admin/CategoryCrudController.php
+++ src/Controller/Admin/CategoryCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Category;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
@@ -15,16 +18,63 @@ class CategoryCrudController extends AbstractCrudController
         return Category::class;
     }
 
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->setEntityLabelInSingular('Catégorie')
+            ->setEntityLabelInPlural('Catégories')
+            ;
+    }
     
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id')->hideOnForm(),
-            TextField::new('designation', 'Désignation'),
-            BooleanField::new('active', 'Actif')
-                ->setTemplatePath('admin/fields/active_field.html.twig')
-                ->onlyOnIndex(),
-            BooleanField::new('active', 'Actif')->onlyOnForms(),
-        ];
+        $id = IdField::new('id') ;
+        $designation = TextField::new('designation', 'Désignation') ;
+        $activeCheckBox = BooleanField::new('active', 'Actif')
+            ->setTemplatePath('admin/fields/active_field.html.twig') ;
+        $active = BooleanField::new('active', 'Actif') ;
+
+        if (Crud::PAGE_INDEX === $pageName) {
+            return [$id, $designation, $activeCheckBox] ;
+        }
+        else {
+            return [$designation, $active] ;
+        }
+    }
+
+    public function configureActions(Actions $actions): Actions
+    {
+        return $actions
+            ->disable(Action::SAVE_AND_CONTINUE)
+            ->disable(Action::SAVE_AND_ADD_ANOTHER)
+            ->update(Crud::PAGE_INDEX, Action::NEW, function (Action $action) {
+                return $action
+                    ->setIcon('fa-solid fa-plus me-1')
+                    ->setLabel('Ajouter') ;
+            })
+            ->update(Crud::PAGE_INDEX, Action::EDIT, function (Action $action) {
+                return $action
+                    ->setIcon('fa-regular fa-pen-to-square me-1 text-warning')
+                    ->setLabel('Modifier') ;
+            })
+            ->update(Crud::PAGE_EDIT, Action::SAVE_AND_RETURN, function (Action $action) {
+                return $action
+                    ->setIcon('fa-solid fa-floppy-disk me-1')
+                    ->setLabel('Sauvegarder') ;
+            })
+            ->update(Crud::PAGE_NEW, Action::SAVE_AND_RETURN, function (Action $action) {
+                return $action
+                    ->setIcon('fa-solid fa-plus me-1')
+                    ->setLabel('Ajouter') ;
+            }) 
+            ->update(Crud::PAGE_INDEX, Action::DELETE, function (Action $action) {
+                return $action
+                    ->setIcon('fa-regular fa-trash-alt me-1 text-danger')
+                    ->setLabel('Supprimer')
+                    ->displayIf(static function ($entity) {
+                        return !count($entity->getDeviceTypes()) ;
+                    }) ;
+            }) 
+            ;
     }
 }
